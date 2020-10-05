c641566ecf14df70391c5b26993880e10b7cfa29
nikita_kogolenok <nikiton17ns@gmail.com>
2020-10-05 15:04:33 +0300

    Initial Commit

---
+++ MyPleces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPleces
+//
+//  Created by Никита Коголенок on 10/5/20.
+//  Copyright © 2020 Никита Коголенок. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
