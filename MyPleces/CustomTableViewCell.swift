595a947048d79caba7a9d9ccf478d927c06b2622
nikita_kogolenok <nikiton17ns@gmail.com>
2020-10-06 11:30:55 +0300

    Custom Table View Cell

---
+++ MyPleces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPleces
+//
+//  Created by Никита Коголенок on 10/6/20.
+//  Copyright © 2020 Никита Коголенок. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+   
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var lacationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
