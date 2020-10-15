74113b9f2634e62157ea19bab8f2e4cc7e264579
nikita_kogolenok <nikiton17ns@gmail.com>
2020-10-15 16:12:55 +0300

    Start work with Map View Controller

---
+++ MyPleces/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  MyPleces
+//
+//  Created by Никита Коголенок on 10/15/20.
+//  Copyright © 2020 Никита Коголенок. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+}
