cacb84f1a79f060dc7908075e3387198603b1ba8
nikita_kogolenok <nikiton17ns@gmail.com>
2020-10-16 12:12:26 +0300

    Setup Placemark and work with Annotations

--- MyPleces/NewPlaceViewController.swift
+++ MyPleces/NewPlaceViewController.swift
@@ -68,16 +68,22 @@ class NewPlaceViewController: UITableViewController {
         }
     }
     
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "showMap" { return }
+        let mapVC = segue.destination as! MapViewController
+        mapVC.place.name = placeName.text!
+        mapVC.place.lacation = placeLocation.text
+        mapVC.place.type = placeType.text
+        mapVC.place.imageData = placeImage.image?.pngData()
+        
+    }
+    
+    
     func savePlace() {
         
-        var image: UIImage?
-        
-        if imageIsChanged {
-            image = placeImage.image
-        } else {
-            image = #imageLiteral(resourceName: "imagePlaceholder")
-        }
-        
+        let image = imageIsChanged ? placeImage.image : #imageLiteral(resourceName: "imagePlaceholder")
+
         let imageData = image?.pngData()
         
         let newPlace = Place(name: placeName.text!,
